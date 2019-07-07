a21822f9b93006fed36966ff5bc2a34e8ce49409
saravanakumar <saravanakumar@SARAVANAN>
2019-07-07 13:46:43 +0530

    Routing

--- Angular8-Start/Angular/src/app/app.module.ts
+++ Angular8-Start/Angular/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path:'Home', component:HomeComponent,
+      },
+      {
+        path:'Employee', component:EmployeeComponent,
+      }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
